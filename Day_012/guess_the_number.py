5437303d3479b308309b5de4a63119bbf69fe494
Vignesh <vignesh.sharma1999@gmail.com>
2025-06-16 16:47:41 +0530

    Add Day 12 - Number Guessing Game Project

---
+++ Day_012/guess_the_number.py
@@ -0,0 +1,32 @@
+import random
+
+EASY_MODE = 10
+HARD_MODE = 5
+
+def guess_number(lives, number):
+    while lives > 0:
+        print(f"You have {lives} attempts to guess the number between 1 & 100.")
+        user_guess = int(input("Type in your guess: "))
+        if user_guess == number:
+            print(f"You guessed it right! The number is {number}!")
+            return
+        else:
+            if user_guess > number:
+                print("The guess is too high!")
+            else:
+                print("The guess is too low!")
+            lives -= 1
+
+    print("You've run out of guesses. Restart the program to play again.")
+    return
+
+chosen_number = random.randint(1, 100)
+
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+
+if difficulty == 'easy':
+    guess_number(EASY_MODE, chosen_number)
+else:
+    guess_number(HARD_MODE, chosen_number)
